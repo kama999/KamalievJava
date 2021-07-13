bd6f5140f2bd218ca617a262a8c6bffa8f40a47b
Kamil Kamaliev <ka-super-kamil@yandex.ru>
2021-07-13 16:12:06 +0300

    created model.py

--- src/Kamaliev.java
+++ src/Kamaliev.java
@@ -1,40 +1,41 @@
 import java.util.Scanner;
 
-public class Kamaliev {
+public class Kamaliev{
 
     public static void main(String[] args) {
-        String name1 = "Вячеслав";
-        System.out.println("Задание 1 :");
-        Scanner scanner1 = new Scanner(System.in);
-        Scanner scanner2 = new Scanner(System.in);
-        Scanner scanner3 = new Scanner(System.in);
+        System.out.println("Задание 1:");
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Введите число > ");
-        int number = scanner1.nextInt();
-        if (number > 7){
+        int number = scanner.nextInt();
+        if (number > 7) {
             System.out.println("Привет");
         }
+        scanner.nextLine();
 
-        System.out.println("Задание 2 : ");
-        System.out.println("Введите имя > ");
-        String name = scanner2.nextLine();
-        if (name.equals(name1)) {
-            System.out.println("Привет, " + name1 );}
-        else {
+        System.out.println("\nЗадание 2: ");
+        System.out.print("Введите имя > ");
+        String name = scanner.nextLine();
+        if (name.equals("Вячеслав")) {
+            System.out.println("Привет, Вячеслав!");
+        } else {
             System.out.println("Нет такого имени");
         }
 
-        System.out.println("Задание 3 : ");
+        System.out.println("\nЗадание 3:");
         System.out.println("Введите размер массива");
-        int size = scanner3.nextInt();
-        int array[] = new int[size];
-        System.out.println("Элементы массива: ");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+        System.out.println("Введите элементы массива:");
         for (int i = 0; i < size; i++) {
-            array[i] = scanner3.nextInt();
+            array[i] = scanner.nextInt();
         }
-        System.out.print ("Числа  кратные 3: ");
+
+        System.out.println("Числа кратные 3:");
         for (int i = 0; i < size; i++) {
-            if (array[i] % 3 == 0){
-        System.out.print (" " + array[i]);
-        }}
+            if (array[i] % 3 == 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
         System.out.println();
-        }}
+    }
+}
